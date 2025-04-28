991d1c2df6b3b1477e8bca321ce028fa8091cc32
Marcelo <devfullmarcelo@gmail.com>
2025-04-27 23:30:36 -0300

    mobile

--- js/notmobile.js
+++ js/notmobile.js
@@ -5,9 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (isMobile) {
         document.body.innerHTML = `
-            <div style="text-align: center; margin-top: 20%; font-family: Arial, sans-serif;">
-                <h1>Galácticos</h1>
-                <p>Galácticos ainda não está disponível para dispositivos móveis.</p>
+            <div style="text-align: center; margin-top: 20%; font-family: Arial, sans-serif; color: #333;">
+            <h1 style="
+                background: linear-gradient(to right, #FFD700, #FFA500);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-size: 3rem;
+                margin-bottom: 1rem;
+            ">
+                Galácticos
+            </h1>
+            <p style="
+                color: #D9534F;
+                font-size: 1.2rem;
+                font-weight: bold;
+            ">
+                Atenção: Galácticos ainda não está disponível para dispositivos móveis.
+            </p>
             </div>
         `;
     }
