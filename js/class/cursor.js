1b840766e43c180af700c64cb28c3fdc241b9270
Marcelo <devfullmarcelo@gmail.com>
2025-04-28 00:33:51 -0300

    init

--- js/class/cursor.js
+++ js/class/cursor.js
@@ -6,55 +6,10 @@ class Player extends Plane {
         this.event();
     }
 
-    up() {
-        if (this.y <= 0) return;
-        this.y -= this.speed;
-    }
-    left() {
-        if (this.x <= 0) return;
-        this.x -= this.speed;
-    }
-    right() {
-        if (this.x + this.w >= config.game.w) return;
-        this.x += this.speed;
-    }
-    down() {
-        if (this.y + this.h >= config.game.h) return;
-        this.y += this.speed;
-    }
-
     event() {
-        const called = callback => {
-            if (!this.run) return;
-            if (this.scene.pauseFlag) return;
-            if (this.scene.game.data.end) return;
-            callback.call(this);
-        };
-
-        // Configuração de teclas para movimentação
-        const keys = {
-            'w': this.up.bind(this),
-            'a': this.left.bind(this),
-            's': this.down.bind(this),
-            'd': this.right.bind(this),
-        };
-        Object.keys(keys).forEach((key) => {
-            hotkey.reg(key, () => {
-                called(keys[key]);
-            }); 
-        });
-
-        // Configuração para disparo com a barra de espaço
-        hotkey.reg(' ', () => {
-            called(() => {
-                res.replay('shoot');
-                this.fire();
-            });
-        }, true);
-
         // Adiciona evento para movimentação com o mouse
         this.scene.canvas.addEventListener('mousemove', (event) => {
-            if (this.run) return;
+            if (!this.run) return;
             if (this.scene.pauseFlag) return;
             if (this.scene.game.data.end) return;
 
